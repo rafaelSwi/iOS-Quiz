a2f40c96cf66638703140a6708e36051dee236de
Rafael Neuwirth Swierczynski <channelrns@gmail.com>
2023-01-08 12:25:45 -0300

    Initial Commit

---
+++ iOS-Quiz/iOS_QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_QuizApp.swift
+//  iOS-Quiz
+//
+//  Created by Rafael Neuwirth Swierczynski on 08/01/23.
+//
+
+import SwiftUI
+
+@main
+struct iOS_QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
